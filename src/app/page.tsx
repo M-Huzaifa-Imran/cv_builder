5c8f75513674b682de824d1e37daad4fbb4dda96
M Huzaifa Imran <mhuzaifaimran106@gmail.com>
2024-10-21 21:32:58 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 
       import CVBuilder from "./cvbuilder"; // Assuming CVBuilder is defined in a separate file//+
 
-      export default function Home() {//+
-        return (//+
+      export default function Home() {
+        return (
           <div>//+
             <CVBuilder/>//+
-          </div>//+
-        );//+
-      }//+
+          </div>
+        )
+      }
 
